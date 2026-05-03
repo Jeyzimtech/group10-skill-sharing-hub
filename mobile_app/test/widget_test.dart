e673f994ea7b9f8a71609d98168d47427bc14418
Obert kakomo <obertkakomo12@gmail.com>
2026-05-03 19:44:36 +0200

    Clean up unused imports in widget_test.dart 2

--- mobile_app/test/widget_test.dart
+++ mobile_app/test/widget_test.dart
@@ -1,21 +1,12 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:mobile_app/main.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const SkillSharingApp());
-
-    // Verify that the login form is shown initially
+    expect(find.textContaining('WELCOME TO THE'), findsOneWidget);
+    await tester.pump(const Duration(seconds: 5));
+    await tester.pumpAndSettle();
     expect(find.text('SIGN IN'), findsOneWidget);
   });
 }

abb6712320c247b0188e7cd7fbdd56f685b1a87a
Obert kakomo <obertkakomo12@gmail.com>
2026-05-05 22:17:30 +0200

    Resolve merge conflicts and integrate Firebase with Authentication UI

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,19 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 import 'loading_screen.dart';
+import 'screens/skill_listing_screen.dart';
 import 'utils/app_colors.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(const SkillSharingApp());
+
+  final isAuthenticated = FirebaseAuth.instance.currentUser != null;
+  runApp(SkillSharingApp(isAuthenticated: isAuthenticated));
 }
 
 class SkillSharingApp extends StatelessWidget {
-  const SkillSharingApp({super.key});
+  final bool isAuthenticated;
+  const SkillSharingApp({super.key, required this.isAuthenticated});
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +42,9 @@ class SkillSharingApp extends StatelessWidget {
         ),
         fontFamily: 'Roboto',
       ),
-      home: const LoadingScreen(),
+      // If already authenticated skip loading/auth screens
+      // TODO: replace Placeholder() with your actual home screen
+      home: isAuthenticated ? const SkillListingScreen() : const LoadingScreen(),
     );
   }
 }
